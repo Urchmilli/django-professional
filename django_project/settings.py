642c9524a343395d86cecfd8c59a813013337507
Uchechukwu Okechukwu <ok.uchechukwu@gmail.com>
2024-05-25 10:42:39 -0500

    ch13

--- django_project/settings.py
+++ django_project/settings.py
@@ -132,6 +132,9 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
